4d6d901d8c9332d8f0aded567f9c2a38ed204aef
Elizabeth Guy <elizabethguy@Elizabeths-MacBook-Pro.local>
2018-10-26 10:44:43 -0700

    add mapping functionality for activity

--- Trying_Things_in_Strava.py
+++ Trying_Things_in_Strava.py
@@ -46,3 +46,48 @@ routes = client_me.get_routes(athlete_id=id)
 #  Result is a dictionary object.  The dict's key are the stream type.
 if 'distance' in streams.keys():
     print(streams['distance'].data)
+
+'''Get list of users and their tokens from MongoDB'''
+mc = MongoClient(host='localhost:47017')
+users = mc['runpaw']
+tokens = list(users.strava_tokens.find())
+
+def make_tokens_list(l):
+    '''Take a list of users from MongoDB and extract their
+       tokens from the dictionary objects in the list'''
+    tokens_list = []
+    unique = []
+    for token in tokens:
+        tokens_list.append(token['token'])
+    for item in tokens_list:
+        if item not in unique:
+            unique.append(item)
+
+    return unique
+
+
+'''Draw activity Map'''
+#get coordinates from activity first
+activity = client.get_activity(activity_id)
+
+activity.to_dict()
+#get polyline to decode for activity of interested
+activity_poly = activity.to_dict()['map']['summary_polyline']
+
+import polyline
+#decode polyline object into coordinates
+coordinates = polyline.decode(activity_poly)
+
+#draw some maps
+import folium
+
+#get start point for map
+lat, long = coordinates[0]
+map = folium.Map(location=[lat, long], zoom_start=12.2)
+
+folium.PolyLine(
+        coordinates,
+        weight=2,
+        color='blue'
+    ).add_to(map)
+map
