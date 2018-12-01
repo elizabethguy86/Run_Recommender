088755dd60a68f26adff8f249676761c7913bea8
Elizabeth Glenn Guy <elizg23@yahoo.com>
2018-12-01 12:19:22 -0800

    improved activities dictionary to take in list of tokens

--- Activities_Dictionary.py
+++ Activities_Dictionary.py
@@ -1,5 +1,4 @@
 from stravalib.client import Client
-from stravalib import unithelper
 import pandas as pd
 
 class Activities:
@@ -7,16 +6,22 @@ class Activities:
     a dictionary object. Converts dictionary object into
     pandas dataframe.'''
 
-    def __init__(self, activites):
+    def __init__(self, tokens, cols):
         #activities from client.get_activities()
-        self.activities = activities
+        self.tokens = tokens 
+        self.cols = cols
 
-    def make_dataframe(self, columns):
-        '''take activities dictionary and convert it to dataframe'''
-        activities = self.activities
-        cols = columns #columns should be in list
-        data = []
-        for activity in activities:
-            activity_dict = activity.to_dict()
-            data.append([activity_dict.get(x) for x in cols])
-        return pd.DataFrame(data, columns=cols)
+    def activities_to_dict(self):
+    data = []
+    tokens = self.tokens
+    for token in tokens:
+    client_current = Client(access_token=token)
+    activities = client_current.get_activities(limit=1000)
+    for activity in activities:
+        my_dict = activity.to_dict()
+        data.append([my_dict.get(x) for x in self.cols])
+    #make large dataframe for columns of interest for all tokens
+    df = pd.DataFrame(data, columns=self.cols)
+    #convert distance to miles
+    df['miles_converted'] = [x/1609.3440122044242 for x in df['distance']]
+    return df
